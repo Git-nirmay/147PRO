69ce026ad71aeede5371fcada9a72eff819e74c9
Git-nirmay <79860151+Git-nirmay@users.noreply.github.com>
2022-04-11 23:58:20 +0530

    Add files via upload

---
+++ 147PRO.py
@@ -0,0 +1,19 @@
+from tkinter import *
+root=Tk()
+root.title("ASCII")
+root.geometry("400x400")
+root.configure(background='blueviolet')
+enter_word=Entry(root)
+enter_word.place(relx=0.5,rely=0.4,anchor=CENTER)
+label=Label(root,text="Name in ASCII",bg="cyan",fg="pink")
+
+
+def AsciiConverter():
+    input_word=enter_word.get()
+    for letter in input_word:
+        label["text"] += str(ord(letter)) +" "
+        
+btn=Button(root,text="Show Name In ASCII",command=AsciiConverter)
+btn.place(relx=0.5,rely=0.5,anchor=CENTER)
+label.place(relx=0.5,rely=0.6,anchor=CENTER)
+root.mainloop()
